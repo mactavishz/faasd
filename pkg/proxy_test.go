ca534ca13dad31100139e552c986cda322b3b385
mactavishz <maczhanchao@yahoo.com>
2026-04-06 21:18:47 +0200

    Fix flaky test

--- pkg/proxy_test.go
+++ pkg/proxy_test.go
@@ -3,11 +3,10 @@ package pkg
 import (
 	"fmt"
 	"io"
-	"log"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"sync"
 	"testing"
 	"time"
 )
@@ -28,50 +27,76 @@ func Test_Proxy_ToPrivateServer(t *testing.T) {
 	}))
 
 	defer upstreamSvr.Close()
-	port := 8080
-	u, _ := url.Parse(upstreamSvr.URL)
-	log.Println("Host", u.Host)
+	u, err := url.Parse(upstreamSvr.URL)
+	if err != nil {
+		t.Fatalf("failed to parse upstream URL: %v", err)
+	}
+
+	port, err := getFreePort("127.0.0.1")
+	if err != nil {
+		t.Fatalf("failed to get free port: %v", err)
+	}
 
 	upstreamAddr := u.Host
-	proxy := NewProxy(upstreamAddr, 8080, "127.0.0.1", time.Second*1, &mockResolver{})
+	proxy := NewProxy(upstreamAddr, uint32(port), "127.0.0.1", time.Second*1, &mockResolver{})
+	errCh := make(chan error, 1)
 
-	gwChan := make(chan string, 1)
-	doneCh := make(chan bool)
-
-	go proxy.Start()
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
 	go func() {
-		gwChan <- u.Host
-		wg.Done()
+		errCh <- proxy.Start()
 	}()
-	wg.Wait()
 
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://127.0.0.1:%d", port), nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+	client := &http.Client{Timeout: 200 * time.Millisecond}
 
-	for i := 1; i < 11; i++ {
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			t.Logf("Try %d, gave error: %s", i, err)
-
-			time.Sleep(time.Millisecond * 100)
-		} else {
-
-			resBody, _ := io.ReadAll(res.Body)
-			if string(resBody) != string(wantBody) {
-				t.Errorf("want %s, but got %s in body", string(wantBody), string(resBody))
-			}
-			break
+	deadline := time.Now().Add(3 * time.Second)
+	var lastErr error
+	for time.Now().Before(deadline) {
+		select {
+		case proxyErr := <-errCh:
+			t.Fatalf("proxy exited early: %v", proxyErr)
+		default:
 		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			lastErr = err
+			time.Sleep(50 * time.Millisecond)
+			continue
+		}
+
+		resBody, readErr := io.ReadAll(res.Body)
+		res.Body.Close()
+		if readErr != nil {
+			t.Fatalf("failed to read response body: %v", readErr)
+		}
+
+		if res.StatusCode != http.StatusOK {
+			lastErr = fmt.Errorf("unexpected status code %d", res.StatusCode)
+			time.Sleep(50 * time.Millisecond)
+			continue
+		}
+
+		if string(resBody) != string(wantBody) {
+			t.Fatalf("want %s, but got %s in body", string(wantBody), string(resBody))
+		}
+
+		return
 	}
 
-	go func() {
-		doneCh <- true
-	}()
+	t.Fatalf("proxy did not become ready before timeout, last error: %v", lastErr)
+}
+
+func getFreePort(host string) (int, error) {
+	l, err := net.Listen("tcp", net.JoinHostPort(host, "0"))
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
 
 type mockResolver struct {
